ea0acd483abd7aa72cd1189464e4b2743cca9a3f
JennTopo <127803618+JennTopo@users.noreply.github.com>
2023-04-12 14:32:55 -0400

    added timer
    added timer but thus far nothing is work 
    need to revisit everything

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ const options = Array.from (document.querySelectorAll('optionsText'));// this re
 const progressText = document.querySelector('#progressText');//this refs line 20 in the game.html
 const scoreText = document.querySelector("#score"); //this ref the line 32 in the game.html
 const progressBarFull = document.querySelector('#progressBarFull'); //this refs the line 24 in the game.html
+const timeEL = document.getElementById('countdown');//this refers to the timer (NEEDS to be added to the HTML)
 
 //these are let statements they allow the element to given a value. ie: boolean, string, number
 let currentQuestion ={}
 let acceptingAnswers = true
 let timer = ('100ms')
-let wronganswer = ('- 10ms')
+let wrongAnswer = ('- 10ms')
+let name = ('input[name=]')
 let score =0
 let questionCounter =0
 let availableQuestions =[]
+
+//Timer countdown 
+function countdown() {
+    timeEL = 30
+}
+let msgInterval = setInterval ('30')
+if (timeLeft > 1) {
+    timerEl.textContent = timeLeft + ' seconds remaining' timeLeft
+} 
+else 
+{ if (timeLeft === 1) 
+    {
+    timerEl.textContent = timeLeft + ' second remaining' timeLeft
+  } else { 
+    timerEl.textContent = 'GAME OVER'
+    clearInterval(timeInterval)
+}
+}
 
 //some where in here I need to add the timer -10 for wrong answers
 // here all all the questions and answers w/o  any action yet
@@ -50,30 +70,29 @@ question: 'What Is The National Animal Of Scotland?', //this refs the line 36 in
     answer3:'Cheetah,'
 },
 ]
-const SCORE_POINTS = 100 //capitalized so it is know as a fixed point
-const MAX_Questions = 4 // this might change based up time 
+//const SCORE_POINTS = 100 //capitalized so it is know as a fixed point
+//const MAX_Questions = 4 // this might change based up time 
 
-// this is the beginning of the game function coding. 
-//Here  is where all the array's and const, that we made are going to start to be used.
+/*this is the beginning of the game function coding. 
+Here is where all the array's and const, that we made are going to start to be used.*/
 startQuiz = () => {  
     questionCounter = 0 // here is where all the elements on the screen are visible
-    score = 0
+//    score = 0
     availableQuestions = [...questions] // here is where you get all the questions in the array
     getNewQuestion()
 }
 //call out to the getNewQuestions
 getNewQuestion =() =>{
     if (availableQuestions.length ===0 || questionCounter > MAX_QUESTIONS) 
-    {
+{
         localStorage.setItem('mostRecentScore,')
         return window.location.assign('/end.html')
-// here is where I need to add the coding for the minus 10 clock
-// also maybe add the right answer if they answer wrong        
+// here is where I need to add the coding for the minus 10 clock also maybe add the right answer if they answer wrong        
 }
 }
 questionCounter++
-progressText.InnerText = 'Question',document.querySelector(questionCounter),document(MAX_QUESTIONS)
-progressBarFull.style.width = On(('click'(questionCounter/MAX_QUESTIONS) * 100%)) // this line will add up how many questions were answered and calculates the score
+progressText.InnerText = document.querySelector.question(questionCounter),document.querySelector(MAX_QUESTIONS)
+//progressBarFull.style.width = On('click'(questionCounter/MAX_QUESTIONS) * 100%) // this line will add up how many questions were answered and calculates the score
 
 const questionIndex = Math.floor(Math, random() * availableQuestions.length) // this is the randomizer (same on I used in my FingerPrintDynamo Repo)
 console.log(options[questionIndex]);
@@ -82,20 +101,48 @@ currentQuestion = availableQuestions(questionIndex) // this keeps track of which
 question.InnerText = currentQuestion.question // this is what question is being asked 
 
 // I do not know what this is linking to had to stop here for now 
-options.fourEach(options) =>;{ 
+options.fourEach(options) =>
     const number = options.dataset[number] // here is where the quiz knows which option button you selected. Refers to lines 41,46,51,56 of the game.HTML
     console.log(optionsButtons)[number]
-    options.InnerText = currentQuestion ['option' + number] //
-}
-//below here are the if / else statements
+    options.InnerText = currentQuestion ['option' + number] // this statement calls out to line 74 in script.js 
+
+/*below here are the if / else statements. I have been trying to look up how to replace an element with out fully removing it and the other day in class a student mentioned splicing. Didn't know 
+what that was so I looked it up*/
 availableQuestions.splice(questionIndex, 1) // by splicing we are Removing elements from an array and, replacing them with new elements in their place, returning the deleted elements.
 // also above line to code calls out to line 
 acceptingAnswers = true /* this is a boolean. which is showing if all coding is correct the choose 
 answer */
 
-//below is now calling out to the actual action of what happens after you GetOptions
-//this selection calls out to the Answer portion of the coding in lines 26,34,402,50 of the quiz.css
+/*below is now calling out to the actual action of what happens after you GetOptions 
+this selection calls out to the Answer portion of the coding in lines 26,34,42,50 of the script.js 
+Wrong answers*/
+options.fourEach(options =>{ 
+options.addEventListener('click'e=>{ // to get it to do a function we have to addEventListener
+        if (!acceptingAnswers) // this statement says if NOT"(!)"" the right answer then ... The "(!)" we learned in class which means NOT  
+      return
 
-options.addEventListener On('click'e=>;{ // to get it to do a function we have to addEventListener
-        if (!acceptingAnswers) return // this statement says if NOT the right answer then ...
-    }) 
+       acceptingAnswers = false
+       const selectedOptions = e.target
+       const selectedAnswer = selectedOptions.dataset['number']
+   //    alert("not correct Try agin You have just lost -10 seconds.Please try again Thank you.");
+
+       //here is the score board / timer replacement
+       let classToApply = selectedAnswer===currentQuestion.answer ?'correct' : 'incorrect'
+      
+       if (classToApply ==='correct'){
+//        incrementScore(SCORE_POINTS)
+}
+
+selectedOptions.parentElement.classList.add(classToApply)
+
+setTimeOut{() => {
+    selectedOptions.parentElement.classList.remove(classToApply)
+    getNewQuestion()
+    
+        }, 1000}
+    })
+})
+incrementScore = num => { 
+    score += numscoreTExt = score
+}
+startGame()
